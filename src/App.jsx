4f157a28aef913ab87017787588d04562cdfe02e
Krishna Parmar <parmarkrishna935@gmail.com>
2025-02-21 00:50:22 +0530

    commit again

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Navbar, Sidebar, Assignments, Cgpa, Courses } from "./index.js";
 import { useState } from "react";
 
